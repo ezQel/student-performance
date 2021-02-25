955d58df463882a16adf72d265748dab3797d422
ezQel <ezekiel3499@gmail.com>
2021-02-25 19:19:52 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RESULTS } from './results';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'student-performance';
+  results = RESULTS;
+  subjectResults = this.results.subject_results;
+  meanMarks = this.results.mean_marks;
+  streamPosition = this.results.stream_position;
+  overallPosition = this.results.overall_position;
+  pastPerformance = this.results.student_performance_over_time;
+  barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true,
+  };
+  barChartLabels = this.pastPerformance.map(p => p.exam_name);
+  barChartType = 'line';
+  barChartLegend = false;
+  barChartData = [
+    {data: this.pastPerformance.map(p => p.avg_score), label: 'Average Score'}
+  ];
+  lineChartColors = [
+    {
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    {
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+    {
+      backgroundColor: 'rgba(255,0,0,0.3)',
+      borderColor: 'red',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+  subjectSelected = false;
+  selectedSubject;
+
+  showSubject(subj): void {
+    this.selectedSubject = subj ? subj : null;
+    this.subjectSelected = !this.subjectSelected;
+  }
+
 }
---
+++ src/app/subject-model.ts
@@ -0,0 +1,9 @@
+export class Subject {
+    rank_out_of: string;
+    subject_name: string;
+    deviation: number;
+    grade: string;
+    comment: string;
+    rank: number;
+    score: number;
+}
